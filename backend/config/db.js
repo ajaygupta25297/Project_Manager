9a282ddd14b23f2b481cb41d93808580252ccd3d
Ajay Kumar Gupta <ajaygupta25297@gmail.com>
2022-07-17 15:28:12 +0530

    add build

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,18 +2,26 @@
 import mongoose from "mongoose";
 import colors from "colors";
 
-const connectDB = async() => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true,
-            useCreateIndex: true,
-        });
-        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
-    } catch (error) {
-        console.error(`Error: ${error.message}`.red.bold);
-        process.exit();
-    }
+const connectDB = async () => {
+  try {
+ mongoose
+      .connect(process.env.MONGO_URI, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex: true,
+      })
+      .then((conn) => {
+        console.log(
+          `MongoDB Connected: ${conn.connection.host}`.cyan.underline
+        );
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  } catch (error) {
+    console.error(`Error: ${error.message}`.red.bold);
+    process.exit();
+  }
 };
 
 export default connectDB;
---
+++ frontend/build/precache-manifest.b85c87050b912409331ca96b8a1cef3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d19e9b5c05e28dffaf1",
+    "url": "/static/css/main.f750d186.chunk.css"
+  },
+  {
+    "revision": "7d19e9b5c05e28dffaf1",
+    "url": "/static/js/main.7d19e9b5.chunk.js"
+  },
+  {
+    "revision": "b3089268092362600429",
+    "url": "/static/js/1.b3089268.chunk.js"
+  },
+  {
+    "revision": "e9d5508c8798565df8f5",
+    "url": "/static/js/2.e9d5508c.chunk.js"
+  },
+  {
+    "revision": "9f3573ee8730cb9f492f",
+    "url": "/static/js/runtime~main.9f3573ee.js"
+  },
+  {
+    "revision": "728ccca3ab12773bc8c2248322632c67",
+    "url": "/static/media/background1.728ccca3.jpg"
+  },
+  {
+    "revision": "d1108953d3499907c1d5f292a4285a77",
+    "url": "/index.html"
+  }
+];
